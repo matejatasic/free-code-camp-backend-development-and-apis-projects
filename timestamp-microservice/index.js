c308eed8d9890bf6c93bfb417ca1a938f8bfb4cb
matejatasic <70795666+matejatasic@users.noreply.github.com>
2024-10-14 16:38:00 +0200

    add timestamp project

---
+++ timestamp-microservice/index.js
@@ -0,0 +1,68 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/:date?", function(request, response) {
+  let date;
+
+  if (!request.params.date) {
+    date = new Date();
+    
+    response.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+
+    return;
+  }
+
+  date = new Date(request.params.date);
+
+  if (isUnixTimestamp(request.params.date)) {
+    date = new Date(parseInt(request.params.date));
+  }
+
+  const isInvalidDate = isNaN(date.getTime());
+
+  if (isInvalidDate) {
+    response.json({
+      error: "Invalid Date"
+    });
+  } 
+
+  response.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
+const isUnixTimestamp = (string) => {
+  return /^\d+$/.test(string);
+}
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
